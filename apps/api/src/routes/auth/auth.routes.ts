a51a560455c6274c1ab94b81356abf2ecd33f1bb
Julian Karhof <julian@karhof.net>
2025-02-12 09:34:13 +0100

    set up better-auth

---
+++ apps/api/src/routes/auth/auth.routes.ts
@@ -0,0 +1,15 @@
+import { auth } from "@api/auth";
+import { type Context, Elysia } from "elysia";
+
+const betterAuthView = (context: Context) => {
+  const BETTER_AUTH_ACCEPT_METHODS = ["POST", "GET"];
+  if (BETTER_AUTH_ACCEPT_METHODS.includes(context.request.method)) {
+    return auth.handler(context.request);
+  } else {
+    context.error(405);
+  }
+};
+
+const authRoute = new Elysia().all("/api/auth/*", betterAuthView);
+
+export default authRoute;
