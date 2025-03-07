b025a4d3120833ba5756602f95ba3df088b6caa8
Julian Karhof <julian@karhof.net>
2025-03-07 14:46:47 +0100

    add use-origin hook

---
+++ apps/web/lib/use-origin.ts
@@ -0,0 +1,14 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function useOrigin() {
+  const [origin, setOrigin] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setOrigin(window.location.origin);
+    }
+  }, []);
+
+  return origin;
+}
