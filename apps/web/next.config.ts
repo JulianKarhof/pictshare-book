b4ed87954882165cbc3b3861b355880a5ebd6f6c
Julian Karhof <julian@karhof.net>
2025-03-22 17:50:56 +0100

    add images production tigris to csp

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -27,6 +27,7 @@ const nextConfig: NextConfig = {
               "*.pict.sh",
               "data:",
               "https://pictshare-book-staging.fly.storage.tigris.dev",
+              "https://pictshare-book.fly.storage.tigris.dev",
               "*.sentry.io",
             ],
             "img-src": [
@@ -36,6 +37,7 @@ const nextConfig: NextConfig = {
               "localhost:*",
               "*.pict.sh",
               "https://pictshare-book-staging.fly.storage.tigris.dev",
+              "https://pictshare-book.fly.storage.tigris.dev",
               "https://cdn.jsdelivr.net",
             ],
             "worker-src": ["'self'", "blob:", "localhost:*", "*.pict.sh"],
